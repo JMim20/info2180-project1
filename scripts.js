cdc92c058b0e1cfcfa6177577ce779f726c2688d
Jamila McGowan <jamcarr280@gmail.com>
2022-10-30 18:44:33 -0700

    Updated css file with rest of layout instruction and uploaded javascript file to make subscribe button functional.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+let subscribeBtn;
+window.onload=function(){
+    subscribeBtn=document.getElementsByClassName("btn")[1];
+    subscribeBtn.addEventListener("click", subscribe);
+}
+let msg;
+let eml;
+
+function subscribe(event){
+    event.preventDefault();
+    msg=document.getElementsByClassName("message");
+    eml=document.getElementById("email").value;
+    
+    if(eml!==""){
+       return msg[0].innerHTML="Thank you! Your email address "+ eml+ " has been added to our mailing list.";
+
+    } else {
+        msg[0].innerHTML="Please enter a valid email address.";
+    }
+    return false;
+}
